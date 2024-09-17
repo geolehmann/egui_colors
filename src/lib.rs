c285a1c29fb98312f9e3fa25d8e943b4c667feb7
Christian Lehmann <clehmann.geo@gmail.com>
2024-09-17 06:02:48 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ pub mod tokens;
 /// Some predefined themes
 pub mod utils;
 
-use eframe::egui;
+use egui;
 use scales::Scales;
 use tokens::{ColorPreset, ColorTokens};
 use utils::{DROPDOWN_TEXT, LABELS, THEMES, THEME_NAMES};
